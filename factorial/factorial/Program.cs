aabe941dac0f24804a139310a4ea7340fe6b0da1
patanrafikhan <iamkhan342@gmail.com>
2023-01-29 12:24:05 +0530

    Added :Facorial Program

---
+++ factorial/factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //factorial
+            Console.WriteLine("Enter the number for factorial:");
+            int n=int.Parse(Console.ReadLine());
+            int value =1;
+            for(int i=0;i>0;i--) 
+            {
+               Console.WriteLine(i);
+
+            }   
+            Console.WriteLine(value);
+         }
+    }
+}
