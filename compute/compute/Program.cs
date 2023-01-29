8d243d93e60abebaafc04ea84dad31e9d34a4f13
patanrafikhan <iamkhan342@gmail.com>
2023-01-29 12:27:42 +0530

    Added :Compute Quotient and Remainder Program

---
+++ compute/compute/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace compute
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=2; int b=3;
+            int z = a + b;
+            Console.WriteLine(z);
+        }
+    }
+}
