69209d63e0803603ac6d37a81ffaaa45c2094736
patanrafikhan <iamkhan342@gmail.com>
2023-01-29 12:30:38 +0530

    Added : Even or Odd Numbers Program

---
+++ evenoroddnumbers/evenoroddnumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace evenoroddnumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter the number =");
+            n=Convert.ToInt32(Console.ReadLine());
+            if ((n&1)==0)
+            {
+                Console.WriteLine(n + " is even");
+            }
+            else
+            {
+                Console.WriteLine(n + "is odd");
+            }
+            Console.ReadKey();
+        }
+    }
+}
