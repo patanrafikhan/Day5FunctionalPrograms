36cdf8d4f09e1fcc98cab82580090a2c08d33362
patanrafikhan <iamkhan342@gmail.com>
2023-01-29 12:22:23 +0530

    Added :Power of 2 Program

---
+++ poweof/poweof/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Powerof
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number power of 2 :");
+            string number = Console.ReadLine();
+            int power = int.Parse(number);
+            int result = 1;
+            while(power>0)
+            {
+                result *=2;
+                power--;
+            }
+            Console.WriteLine("The Power of result is :" + result);
+        }
+    }
+}
