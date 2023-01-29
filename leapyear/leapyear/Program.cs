ff54e48fc0f3410b43474a46f4ff7d4704326a38
patanrafikhan <iamkhan342@gmail.com>
2023-01-29 12:20:58 +0530

    Added :Leapyear Program

---
+++ leapyear/leapyear/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leapyear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter year :")
+               if (((year % 400==0)) && (year % 100==0) && (year % 4==0))
+            {
+                Console.Write("Year is leap Year ");
+
+            }
+            else
+            {
+                Console.WriteLine("this is not a leapyear :);")
+            }
+        }
+    }
+}
