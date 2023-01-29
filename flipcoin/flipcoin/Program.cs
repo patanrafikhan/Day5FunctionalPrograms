b45351c88ee66158d6e4644701b3593bedd5480b
patanrafikhan <iamkhan342@gmail.com>
2023-01-29 12:18:56 +0530

    Added :Flipcoin Program

---
+++ flipcoin/flipcoin/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace flipcoin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+
+
+                Random ran = new Random();
+                int num = ran.Next(0, 2);
+                if (num == 0)
+                {
+                    Console.WriteLine("Head");
+                }
+                else
+                {
+                    Console.WriteLine("Tail");
+                }
+            }
+        }
+
+    }
+}
